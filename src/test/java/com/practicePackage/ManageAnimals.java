128c62a2caad0b2790eff79a995d387a7c1e1128
Mr. Santosh <Mr. Santosh@Digisol>
2023-01-19 14:38:04 +0530

    branch-1 2nd commit

---
+++ src/test/java/com/practicePackage/ManageAnimals.java
@@ -0,0 +1,13 @@
+package com.practicePackage;
+
+import org.testng.annotations.Test;
+
+public class ManageAnimals {
+
+	@Test
+	public void manageAnimals()
+	{
+		System.out.println("add animals");
+	}
+}
+
