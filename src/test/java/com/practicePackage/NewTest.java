63d5d7fc715432eefafdff230e4ee6df1581520f
Mr. Santosh <Mr. Santosh@Digisol>
2023-01-19 11:59:27 +0530

    this my 2nd commit

---
+++ src/test/java/com/practicePackage/NewTest.java
@@ -0,0 +1,13 @@
+package com.practicePackage;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+	@Test
+	public void newTest()
+	{
+		System.out.println("this is a new Test");
+	}
+
+}
